bf95816437777c46431bde378cea804c9079c765
AlexandruRoventa <alex.roventa94@gmail.com>
2024-08-10 19:06:33 +0300

    add external links for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    HOST_URI: process.env.HOST_URI,
+
+    MONGO_URI: process.env.MONGO_URI,
+  },
   images: {
     remotePatterns: [
       {
--- src/_lib/rooms/getRooms.ts
+++ src/_lib/rooms/getRooms.ts
@@ -1,4 +1,5 @@
 export async function getRooms() {
+  
   try {
     const response = await fetch(`${process.env.HOST_URI}/api/rooms`, {
       next: { revalidate: 86400 },
